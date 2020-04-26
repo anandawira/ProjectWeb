b037213d6f5dce882dd1a3fb8ded0b17cde70ca4
AnandaWira <anandawira754@gmail.com>
2020-04-26 20:21:23 +0700

    makes all text align middle

--- pages/master_data/department.php
+++ pages/master_data/department.php
@@ -23,35 +23,35 @@
             <table class="table table-hover mx-auto m-0 w-auto text-center">
                 <thead class="thead-dark">
                     <tr>
-                    <th scope="col" class="text-center">No</th>
-                    <th scope="col">Department ID</th>
-                    <th scope="col">Department Name</th>
-                    <th scope="col" class="text-center">Action</th>
+                    <th scope="col" class="align-middle">No</th>
+                    <th scope="col" class="align-middle">Department ID</th>
+                    <th scope="col" class="align-middle">Department Name</th>
+                    <th scope="col" class="align-middle">Action</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <th scope="row">1</th>
-                        <td>101</td>
-                        <td>Finance</td>
+                        <th class="align-middle" scope="row">1</th>
+                        <td class="align-middle">101</td>
+                        <td class="align-middle">Finance</td>
                         <td class="py-0 align-middle">
                             <button type="button" class="btn btn-secondary btn-sm mx-1">Edit</button>
                             <button type="button" class="btn btn-danger btn-sm mx-1">Delete</button>
                         </td>
                     </tr>
                     <tr>
-                        <th scope="row">2</th>
-                        <td>102</td>
-                        <td>Marketing</td>
+                        <th class="align-middle" scope="row">2</th>
+                        <td class="align-middle">102</td>
+                        <td class="align-middle">Marketing</td>
                         <td class="py-0 align-middle">
                         <button type="button" class="btn btn-secondary btn-sm mx-1">Edit</button>
                         <button type="button" class="btn btn-danger btn-sm mx-1">Delete</button>
                         </td>
                     </tr>
                     <tr>
-                        <th scope="row">3</th>
-                        <td>103</td>
-                        <td>Production</td>
+                        <th class="align-middle" scope="row">3</th>
+                        <td class="align-middle">103</td>
+                        <td class="align-middle">Production</td>
                         <td class="py-0 align-middle">
                         <button type="button" class="btn btn-secondary btn-sm mx-1">Edit</button>
                         <button type="button" class="btn btn-danger btn-sm mx-1">Delete</button>

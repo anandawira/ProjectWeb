3fb968c344ed48427762a79ab4c27404a5ec9458
AnandaWira <anandawira754@gmail.com>
2020-04-26 20:01:45 +0700

    done making table ui, not linked to database yet.

---
+++ koneksi.php
@@ -0,0 +1,10 @@
+<?php 
+//$koneksi = mysqli_connect("localhost","id13127704_root","%v6UE!-IX?UrpK|6","id13127704_absensikaryawan");
+$koneksi = mysqli_connect("localhost","root","","absensikaryawan");
+
+// Check connection
+if (mysqli_connect_errno()){
+	echo "Koneksi database gagal : " . mysqli_connect_error();
+}
+
+?>

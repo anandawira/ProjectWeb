007f9c74763f1059d85800bf3c496f91759b3791
AnandaWira <anandawira754@gmail.com>
2020-04-26 23:13:42 +0700

    department CRUD

--- pages/master_data/department.php
+++ pages/master_data/department.php
@@ -16,11 +16,11 @@
     ></script>
 </head>
 <body>
-    <?php
-        include '../../koneksi.php';
-    ?>
-        <div class="table-responsive mt-2">
-            <table class="table table-hover mx-auto m-0 w-auto text-center">
+    <h2 class="text-center mt-5 mb-0">DEPARTMENT MASTER DATA</h2>
+    <div class="container-sm p-0 mb-5">
+    <button type="button" class="btn btn-primary btn-sm mb-2 float-right"><i class="fas fa-plus"></i>  Add</button>
+        <div class="table-responsive">
+            <table class="table table-hover mx-auto m-0  text-center">
                 <thead class="thead-dark">
                     <tr>
                     <th scope="col" class="align-middle">No</th>
@@ -30,35 +30,28 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <th class="align-middle" scope="row">1</th>
-                        <td class="align-middle">101</td>
-                        <td class="align-middle">Finance</td>
-                        <td class="py-0 align-middle">
-                            <button type="button" class="btn btn-secondary btn-sm mx-1">Edit</button>
-                            <button type="button" class="btn btn-danger btn-sm mx-1">Delete</button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th class="align-middle" scope="row">2</th>
-                        <td class="align-middle">102</td>
-                        <td class="align-middle">Marketing</td>
-                        <td class="py-0 align-middle">
-                        <button type="button" class="btn btn-secondary btn-sm mx-1">Edit</button>
-                        <button type="button" class="btn btn-danger btn-sm mx-1">Delete</button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th class="align-middle" scope="row">3</th>
-                        <td class="align-middle">103</td>
-                        <td class="align-middle">Production</td>
-                        <td class="py-0 align-middle">
-                        <button type="button" class="btn btn-secondary btn-sm mx-1">Edit</button>
-                        <button type="button" class="btn btn-danger btn-sm mx-1">Delete</button>
-                        </td>
-                    </tr>
+                    <?php
+                        $koneksi = mysqli_connect("localhost","root","","absensikaryawan");
+                        $no = 1;
+                        $data = mysqli_query($koneksi,"select * from department");
+                        while($d = mysqli_fetch_array($data)){
+                    ?>
+                        <tr>
+                            <th class="align-middle" scope="row"><?php echo $no++; ?></th>
+                            <td class="align-middle"><?php echo $d['id']; ?></td>
+                            <td class="align-middle"><?php echo $d['name']; ?></td>
+                            <td class="py-1 align-middle">
+                                <button type="button" class="btn btn-secondary btn-sm mx-1"><i class="fas fa-edit"></i>  Edit</button>
+                                <button type="button" class="btn btn-danger btn-sm mx-1"><i class="fas fa-trash-alt"></i>  Delete</button>
+                            </td>
+                        </tr>
+
+                    <?php 
+                        }
+                    ?>
                 </tbody>
             </table>
         </div>
+    </div>
 </body>
 </html>

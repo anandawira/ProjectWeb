919d1309e474bfd006e211ab347890ae36b160ab
AnandaWira <anandawira754@gmail.com>
2020-05-03 22:37:02 +0700

    belum bisa auto refresh

--- pages/master_data/crud_department/insert_department.php
+++ pages/master_data/crud_department/insert_department.php
@@ -5,5 +5,6 @@ include $root.'/koneksi.php';
 if (!empty($_POST)) {
     $name = mysqli_real_escape_string($koneksi, $_POST["departmentName"]);
     mysqli_query($koneksi,"CALL add_department('$name')");
+    header('Location: ../department.php'); //Later add page=dept
 }
 ?>

9ba06924d32492c3708837554ed9d22bd4cf76af
AnandaWira <anandawira754@gmail.com>
2020-05-22 21:30:28 +0700

    dashboard ui fix

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -1,4 +1,4 @@
-<div class="container mt-5">
+<div class="container mt-2">
           <!-- Content Row -->
           <div class="row">
 
@@ -13,8 +13,8 @@
                     </div>
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
-                        <div class="h4 font-weight-bold text-primary text-uppercase text-center mt-2 mb-1">Employee (Active)</div>
-                        <div class="h5 my-2 font-weight-bold text-primary text-center display-2">
+                        <div class="h4 font-weight-bold text-primary text-uppercase text-center mt-2">Employee</div>
+                        <div class="my-2 font-weight-bold text-primary text-center display-4">
                         <?php
                             $root =  $_SERVER['DOCUMENT_ROOT'];
                             include $root.'/koneksi.php';   
@@ -40,8 +40,8 @@
                     </div>
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                      <div class="h4 font-weight-bold text-success text-uppercase text-center mt-2 mb-1">Department</div>
-                      <div class="h5 my-2 font-weight-bold text-success text-center display-2">
+                      <div class="h4 font-weight-bold text-success text-uppercase text-center mt-2">Department</div>
+                      <div class="h5 my-2 font-weight-bold text-success text-center display-4">
                         <?php
                         $data = mysqli_query($koneksi,"SELECT * FROM department_count");
                         $result= $data->fetch_row();
@@ -64,8 +64,8 @@
                     </div>
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                      <div class="h4 font-weight-bold text-info text-uppercase text-center mt-2 mb-1">Shift</div>
-                      <div class="my-2 font-weight-bold text-info text-center display-2">
+                      <div class="h4 font-weight-bold text-info text-uppercase text-center mt-2">Shift</div>
+                      <div class="my-2 font-weight-bold text-info text-center display-4">
                         <?php
                         $data = mysqli_query($koneksi,"SELECT * FROM shift_count");
                         $result= $data->fetch_row();

4438217be0b2e48852524e04fb74b4f748910474
AnandaWira <anandawira754@gmail.com>
2020-05-22 15:32:09 +0700

    add sql file

--- index.php
+++ index.php
@@ -3,7 +3,30 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Dashboard</title>
+    <title>
+    <?php
+        $page = isset($_GET['page'])?$_GET['page']:'';
+        if($page=="employee"){
+          echo "Employee Master Data";
+        }elseif ($page=="department") {
+          echo "Department Master Data";
+        }elseif ($page=="location") {
+          echo "Location Master Data";
+        }elseif ($page=="shift") {
+          echo "Shift Master Data";
+        }elseif($page=="login_data"){
+          echo "Login Master Data";
+        }elseif($page=="daily_report"){
+          echo "Attendance Report";
+        }elseif($page=="late_report"){
+          echo "Late Attendance Report";
+        }elseif($page=="incomplete_report"){
+          echo "Incomplete Attendance Report";
+        }else{
+          echo "";
+        }
+      ?>
+    </title>
     <script src="https://code.jquery.com/jquery-3.5.0.js" integrity="sha256-r/AaFHrszJtwpe+tHyNi/XCfMxYpbsRg2Uqn0x3s2zc=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
